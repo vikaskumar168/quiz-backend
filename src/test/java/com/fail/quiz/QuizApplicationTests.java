cb343a2ffcfd62442e0302e9b1f77194b448d4bd
vikas <vikaskmr2411@gmail.com>
2025-11-28 00:58:36 +0530

    Initial Files

---
+++ src/test/java/com/fail/quiz/QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fail.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
